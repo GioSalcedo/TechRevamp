1e7103cd0f8238bc9c3b34bbf31865a85d16af05
Nicole Lorena Rojas Ramirez <nicole.rojas@genstudents.org>
2024-05-28 15:06:52 -0500

    cambios de nav en phone (responsive aun pendiente)

--- navbar-footer.js
+++ navbar-footer.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', function () {
 // navbar
 // Toggle the active state of the menu when the toggle button is clicked
 function toggleMenu() {
-  const navbar = document.querySelector('.navbar-right');
-  const search = document.querySelector('.navbar-center');
+  const navbar = document.querySelector('.menu');
+  const search = document.querySelector('.searchbar');
 
   navbar.classList.toggle('active');
   search.classList.toggle('hidden');
