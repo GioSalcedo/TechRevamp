967f0d4fbd395ec5fabd8132820339d0ba7e73c7
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-05-31 01:53:24 -0500

    added nodemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Importa el módulo 'express' para crear una aplicación web
 const express = require("express")
-require('ejs')
+require('ejs ')
 
 // Crea una instancia de la aplicación Express
 const app = express();
@@ -8,6 +8,7 @@ const Routes = require("./src/routes/routes.js")
 
 // Configuraciones del motor que usaremos
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
 
 app.use(Routes)
 // Configura la aplicación para servir archivos estáticos desde el directorio "public"
