6c655a6e21a8ab4733cf30c1aa2fbbf90bf291dc
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-06-03 14:30:02 -0500

    card styles in all media queries

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,2 @@
 // js general
-import { setupScrollAnimations } from './animationCards.js';
-import { setupCardEvents } from './eventsCards.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  setupScrollAnimations();
-  setupCardEvents();
-});
-
 console.log("Hello")
