4d08291b89ec35d10eb1763adb1db06da415ca43
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-06-03 16:18:26 -0500

    fix help syntx

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -20,6 +20,10 @@ router.get("/contactanos", (req, res) => {
   res.render('contact-form');
 });
 
+router.get("/ayuda", (req, res) => {
+  res.render('help');
+});
+
 // Manejo de rutas no encontradas (404)
 router.use((req, res) => {
   res.status(404).send('No se encontró esta página');
