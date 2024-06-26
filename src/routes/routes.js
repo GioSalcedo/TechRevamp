c43f5b835535c91b4227acbe04e3ca7b5161f0ae
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-06-26 06:33:34 -0500

    fixed routes of products

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -92,6 +92,10 @@ router.get('/productos-parcial', (req, res) => {
 });
 
 router.get('/productos/:id', (req, res) => {
+  const file = fs.readFileSync('api/products.json', 'UTF-8');
+  const json = JSON.parse(file);
+  const productos = json.productos;
+
   const productId = req.params.id;
   const product = productos.find(p => p.id == productId);
 
