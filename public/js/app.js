8837e013aea57f7fef4c1bef8b63c839c1b6b0fc
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-06-26 13:16:25 -0500

    added button up

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-// js general
+window.addEventListener('scroll', function() {
+  const scrollToTopButton = document.getElementById('scrollToTopButton');
+  if (window.scrollY > 200) {
+    scrollToTopButton.classList.add('show-scroll-to-top');
+  } else {
+    scrollToTopButton.classList.remove('show-scroll-to-top');
+  }
+});
